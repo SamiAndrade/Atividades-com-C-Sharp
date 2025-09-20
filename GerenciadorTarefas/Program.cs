476efb56ff3ee3ad52c0481c57e644ab2b4c51ac
Sami Carvalho <samigabriele1@gmail.com>
2025-09-20 11:41:47 -0300

    first commit

---
+++ GerenciadorTarefas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Queue<string> tarefasPendentes = new Queue<string>();
+    static Stack<string> tarefasConcluidas = new Stack<string>();
+
+    static void Main(string[] args)
+    {
+        int opcao;
+        do
+        {
+            
+        } while (opcao != 0);
+    }
+}
