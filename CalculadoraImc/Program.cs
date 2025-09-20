0940cb4c512f2a85a7b711dd3914c7b23ed57f04
Sami Carvalho <samigabriele1@gmail.com>
2025-09-19 23:07:02 -0300

    first commit

---
+++ CalculadoraImc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**** Calculadora de IMC ****\n");
+
+        string Nome = ObterNome();
+        double Peso = ObterNumero("Digite seu peso (em kg): ");
+        double Altura = ObterNumero("Digite sua altura (em metros): ");
+
+        double Imc = CalcularImc(Peso, Altura);
+        string Classificacao = ClassificarImc(Imc);
+
+
+        Console.WriteLine($"O seu nome é: {Nome}, possui o peso de {Peso} kilos e uma altura de {Altura} metros. \n Seu IMC é de: {Imc:F2}");
+        Console.WriteLine($"Classificação: {Classificacao}");
+
+    }
+
+    static string ObterNome() {
+        Console.WriteLine("Digite seu nome: ");
+        return Console.ReadLine();
+    }
+
+    static double ObterNumero(string mensagem) {
+        double numero;
+        Console.WriteLine(mensagem);
+        while (!double.TryParse(Console.ReadLine().Replace(",", "."), NumberStyles.Number, CultureInfo.InvariantCulture, out numero) || numero <= 0)
+        {
+            Console.Write("Valor inválido. Tente novamente: ");
+        }
+
+        return numero;
+    }
+
+    static double CalcularImc(double peso, double altura) {
+        return peso / (altura * altura);
+    }
+
+    static string ClassificarImc(double imc) {
+        if (imc < 18.5)
+            return "Abaixo do peso";
+        else if (imc < 25)
+            return "Peso normal";
+        else if (imc < 30)
+            return "Sobrepeso";
+        else if (imc < 35)
+            return "Obesidade grau I";
+        else if (imc < 40)
+            return "Obesidade grau II";
+        else
+            return "Obesidade grau III (mórbida)";
+    }
+}
---
+++ CalculadoraImc/README.md
@@ -0,0 +1,29 @@
+# 🧮 Calculadora de IMC - C# .NET Console
+
+Este é um projeto simples de calculadora de IMC (Índice de Massa Corporal) feito em **C#** com **.NET**.
+
+## 💡 Funcionalidades
+
+- Solicita nome do usuário
+- Lê peso (em kg) e altura (em metros)
+- Calcula o IMC
+- Informa a classificação de acordo com a tabela da OMS
+- Valida as entradas (não aceita valores inválidos)
+
+
+▶️ Como executar:
+
+1. Tenha o SDK do .NET 6+
+ instalado
+
+2. Clone o repositório:
+```
+git clone https://github.com/seu-usuario/calculadora-imc-csharp.git
+cd calculadora-imc-csharp
+
+```
+
+3. Execute depois usando o comando: 
+```
+dotnet run
+```
