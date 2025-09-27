04021ba1ff488272caec5d43fefc83e717927f53
Sami Carvalho <samigabriele1@gmail.com>
2025-09-26 23:39:51 -0300

    feat: desafio solucionado

---
+++ ExercicioPoo-DesafioDio/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioPOO.Models;
+
+Smartphone celular1 = new Nokia("1199999999", "Nokia", "111222333444555", 16);
+Smartphone celular2 = new Iphone("1199999999", "Iphone 13", "111222333444555", 64);
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("Snake");
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Instagram");
